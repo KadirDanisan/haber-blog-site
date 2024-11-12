9de1d8b35ae3e78e15f58f41218a9bbf81f48b26
Kadir DANIŞAN <kadirdansan06@gmail.com>
2024-11-12 22:36:39 +0300

    js page finish commit

--- react_pages/src/App.js
+++ react_pages/src/App.js
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignUp/LoginSignup';
-// import Categories from './Components/Categories/Categories';
+import Categories from './Components/Categories/Categories';
 
 function App() {
-  // const categoriesData = [
-  //   { id: 1, name: 'Teknoloji' },
-  //   { id: 2, name: 'Spor' },
-  //   { id: 3, name: 'Sağlık' },
-  //   ];
+  const categoriesData = [
+    { id: 1, name: 'Teknoloji' },
+    { id: 2, name: 'Spor' },
+    { id: 3, name: 'Sağlık' },
+    ];
   return (
     <div>
        <LoginSignup/>
-       {/* <Categories categories={categoriesData} /> */}
+       <Categories categories={categoriesData} />
     </div>
   );
 }
